accd84bbef3a051e225052735f4b605a0bf72d63
Julien Kosinski <julien.kosinski@gmail.com>
2019-07-18 16:26:58 +0200

    stubs and spies tests

--- nodejs/verifySomeBehavior/test/BirthdayService.spec.js
+++ nodejs/verifySomeBehavior/test/BirthdayService.spec.js
@@ -3,14 +3,37 @@
 var expect = require('chai').expect;
 let sinon = require('sinon')
 
-describe('BirthdayService', function() {
+describe('BirthdayService', function () {
     var BirthdayService = require('../src/BirthdayService');
+    var sinon = require('sinon')
 
-    describe('greeting()', function() {
-        it('does something', function() {
+    describe('greeting()', function () {
+        //given
+        let myClientRepository = {
+            birthdayIsTodayFor: sinon.stub()
+        }
+        myClientRepository.birthdayIsTodayFor.withArgs('fred').returns(true)
+        let mailer = {
+            send: sinon.spy()
+        }
 
-//            use sinon.assert.calledWith(spy, "...");
+        let b = new BirthdayService(myClientRepository, mailer)
 
+       //when
+        b.greeting('fred')
+        it('does something', function () {
+            //then
+            sinon.assert.calledWith(mailer.send, "Happy birthday fred!");
+        });
+        it('greetings match', function () {
+            sinon.assert.calledWith(mailer.send, sinon.match.string)
+        });
+        it('greetings get arg 0', function () {
+            //var tmp = mailer.send.getCall(0).args[0]
+            var tmp = mailer.send.firstCall.args[0]
+            //expect(tmp).equal("Happy birthday fred!")
+            expect(tmp).include("Happy bir")
+            //sinon.assert.calledWith(mailer.send, "Happy birthday fred!");
         });
     });
 });
